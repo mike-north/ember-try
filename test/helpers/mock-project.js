804f5d12bd0e8840477f42e03aa271641e6cb270
Mike North <northm@yahoo-inc.com>
2015-08-03 10:57:52 -0700

    package.json scenario/revert + tests

--- test/helpers/mock-project.js
+++ test/helpers/mock-project.js
@@ -5,6 +5,7 @@ var _rimraf       = require('rimraf');
 var spawnSync     = require('child_process').spawnSync;
 
 var fixtureBower  = require('../fixtures/bower.json');
+var fixtureNpm    = require('../fixtures/package.json');
 
 var rimraf        = RSVP.denodeify(_rimraf);
 var mkdir         = RSVP.denodeify(fs.mkdir);
@@ -18,33 +19,61 @@ function MockProject(root) {
 }
 
 MockProject.prototype = {
+  _writeFixtureDataToProject: function (fixture, filename) {
+    return writeFile(this.projectRoot + '/' + filename, JSON.stringify(fixture, null, 2));
+  },
+
+  _setupBower: function () {
+    this._writeFixtureDataToProject(fixtureBower, 'bower.json').then(function () {
+      return spawnSync('bower', ['install'], {
+        cwd: this.projectRoot
+      }.bind(this));
+    });
+  },
+  _setupNpm: function () {
+    this._writeFixtureDataToProject(fixtureNpm, 'package.json').then(function () {
+      return spawnSync('npm', ['install'], {
+        cwd: this.projectRoot
+      }.bind(this));
+    });
+  },
+
   setup: function () {
     var mockProj = this;
     return rimraf(mockProj.projectRoot).then(function () {
       return mkdir(mockProj.projectRoot).then(function () {
-        return writeFile(mockProj.projectRoot + '/bower.json', JSON.stringify(fixtureBower)).then(function () {
-          return spawnSync('bower', ['install'], {
-            cwd: mockProj.projectRoot
-          });
-        });
-      });
-    });
+        return RSVP.all([
+          this._setupNpm(),
+          this._setupBower()
+        ]);
+      }.bind(this));
+    }.bind(this));
   },
 
   destroy: function () {
     return rimraf(this.projectRoot);
   },
 
-  bowerData: function() {
-    return readFile(this.projectRoot + '/bower.json').then(function(fileData) {
+  _jsonFileData: function (filename) {
+    return readFile(this.projectRoot + '/' + filename).then(function(fileData) {
       return JSON.parse(fileData);
     });
   },
 
+  bowerData: function() {
+    return this._jsonFileData('bower.json');
+  },
+
+  npmData: function() {
+    return this._jsonFileData('package.json');
+  },
+
   backupBowerData: function () {
-    return readFile(this.projectRoot + '/bower.json.ember-try').then(function(fileData) {
-      return JSON.parse(fileData);
-    });
+    return this._jsonFileData('bower.json.ember-try');
+  },
+
+  backupNpmData: function () {
+    return this._jsonFileData('package.json.ember-try');
   },
 
   createBowerBackup: function (packageVersions) {
@@ -55,6 +84,17 @@ MockProject.prototype = {
         bowerJson.dependencies[k] = vers[k];
       }
       return writeFile(mockProj.projectRoot + '/bower.json.ember-try', JSON.stringify(bowerJson));
+    });
+  },
+
+  createNpmBackup: function (packageVersions) {
+    var vers = packageVersions || {};
+    var mockProj = this;
+    return this.npmData().then(function(packageJson){
+      for(var k in vers) {
+        packageJson.dependencies[k] = vers[k];
+      }
+      return writeFile(mockProj.projectRoot + '/package.json.ember-try', JSON.stringify(packageJson));
     });
   }
 };
---
+++ test/npm-helpers-test.js
@@ -0,0 +1,64 @@
+var npmHelpers = require('../lib/utils/npm-helpers');
+var should = require("should");
+
+var MockProject = require('./helpers/mock-project');
+
+describe.only('npmHelpers', function(){
+
+  describe('#resetNpmFile()', function() {
+    // actual npm install could take a while
+    this.timeout(10000);
+
+    var proj = new MockProject('.scratch');
+    beforeEach(function () {
+      return proj.setup();
+    });
+
+    afterEach(function() {
+      return proj.destroy();
+    });
+
+    it('should replace a package.json with the package.json.ember-try', function(done) {
+      var proj = new MockProject('.scratch');
+
+      proj.createNpmBackup({
+        'jquery': '2.1.3'
+      }).then(function() {
+        npmHelpers.resetNpmFile(process.cwd() + '/.scratch').then(function () {
+          proj.npmData().then(function(data) {
+            data.dependencies.jquery.should.equal('2.1.3');
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('#backupNpmFile()', function() {
+    // actual npm install could take a while
+    this.timeout(10000);
+
+    var proj = new MockProject('.scratch');
+    beforeEach(function () {
+      return proj.setup();
+    });
+
+    afterEach(function() {
+      return proj.destroy();
+    });
+
+    it('should copy package.json to package.json.ember-try', function(done) {
+      proj.createNpmBackup({
+        'jquery': '2.1.3'
+      }).then(function() {
+        npmHelpers.backupNpmFile(process.cwd() + '/.scratch').then(function () {
+          proj.backupNpmData().then(function(npmData) {
+            npmData.dependencies.jquery.should.equal('^1.11.1');
+            done();
+          });
+        });
+      });
+    });
+  });
+
+})
